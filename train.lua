f1eef616b25b9150a385bcea1f477a5f8a708c54
Hamed Hassanisaadi <hassanih@mcmaster.ca>
2018-06-17 19:44:44 -0400

    removed fromfile() from train and test and added utils.lua.

--- train.lua
+++ train.lua
@@ -5,6 +5,7 @@ require 'torch'
 require 'optim'
 require 'models'
 require 'image'
+require 'utils'
 --require 'loadcaffe'
 
 io.stdout:setvbuf('no')
@@ -45,55 +46,6 @@ if opt.g then
   --vgg = loadcaffe.load(opt.vggProto, opt.vggmodel, 'cudnn')
 else
   --vgg = loadcaffe.load(opt.vggProto, opt.vggmodel, 'nn')
-end
-
-
-function fromfile(fname)
-   local file = io.open(fname .. '.dim')
-   local dim = {}
-   for line in file:lines() do
-      table.insert(dim, tonumber(line))
-   end
-   if #dim == 1 and dim[1] == 0 then
-      return torch.Tensor()
-   end
-
-   local file = io.open(fname .. '.type')
-   local type = file:read('*all')
-
-   local x
-   local s
-   if type == 'float32' then
-      s = 1
-      for i=1,#dim do
-         s = s * dim[i]
-      end
-      --x = torch.FloatTensor(torch.FloatStorage(fname))
-      x = torch.FloatTensor(torch.FloatStorage(s))
-      torch.DiskFile(fname,'r'):binary():readFloat(x:storage())
-   elseif type == 'int32' then
-      s = 1
-      for i=1,#dim do
-         s = s * dim[i]
-      end
-      --x = torch.IntTensor(torch.IntStorage(fname))
-      x = torch.IntTensor(torch.IntStorage(s))
-      torch.DiskFile(fname,'r'):binary():readInt(x:storage())
-   elseif type == 'int64' then
-      s = 1
-      for i=1,#dim do
-         s = s * dim[i]
-      end
-      --x = torch.LongTensor(torch.LongStorage(fname))
-      x = torch.LongTensor(torch.LongStorage(s))
-      torch.DiskFile(fname, 'r'):binary():readLong(x:storage())
-   else
-      print(fname, type)
-      assert(false)
-   end
-
-   x = x:reshape(torch.LongStorage(dim))
-   return x
 end
 
 -- Loading train data
@@ -145,8 +97,8 @@ print(net)
 params, gradParams = net:getParameters()
 local optimState = {
                     learningRate = opt.lr,
---                    learningRateDecay = 0,
---                    weightDecay = 0,
+                    learningRateDecay = 0,
+                    weightDecay = 10e-6,
                     beta1 = opt.beta1,
                     beta2 = opt.beta2,
                     epsilon = 10e-8
---
+++ utils.lua
@@ -0,0 +1,50 @@
+#! /usr/bin/env luajit
+
+function fromfile(fname)
+   local file = io.open(fname .. '.dim')
+   local dim = {}
+   for line in file:lines() do
+      table.insert(dim, tonumber(line))
+   end
+   if #dim == 1 and dim[1] == 0 then
+      return torch.Tensor()
+   end
+
+   local file = io.open(fname .. '.type')
+   local type = file:read('*all')
+
+   local x
+   local s
+   if type == 'float32' then
+      s = 1
+      for i=1,#dim do
+         s = s * dim[i]
+      end
+      --x = torch.FloatTensor(torch.FloatStorage(fname))
+      x = torch.FloatTensor(torch.FloatStorage(s))
+      torch.DiskFile(fname,'r'):binary():readFloat(x:storage())
+   elseif type == 'int32' then
+      s = 1
+      for i=1,#dim do
+         s = s * dim[i]
+      end
+      --x = torch.IntTensor(torch.IntStorage(fname))
+      x = torch.IntTensor(torch.IntStorage(s))
+      torch.DiskFile(fname,'r'):binary():readInt(x:storage())
+   elseif type == 'int64' then
+      s = 1
+      for i=1,#dim do
+         s = s * dim[i]
+      end
+      --x = torch.LongTensor(torch.LongStorage(fname))
+      x = torch.LongTensor(torch.LongStorage(s))
+      torch.DiskFile(fname, 'r'):binary():readLong(x:storage())
+   else
+      print(fname, type)
+      assert(false)
+   end
+
+   x = x:reshape(torch.LongStorage(dim))
+   return x
+end
+
